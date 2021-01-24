93d64a7f1f8deff156aaec2165b14dae9c2bb933
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-01-24 12:05:56 +0100

    OAuth2 authorization server implemented

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+import java.io.Serializable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService, Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null ) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("User not found");
+		}
+		logger.info("User found: " + username);
+		return user;
+	}
+
+}
+ 
